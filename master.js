301712a927ddd21750bf9bf5a4ab1721a3f491b6
PineapplesOfJustice <gre8nthu7der@gmail.com>
2018-08-19 11:49:25 -0700

    Major updates. Finish the webpages.

--- master.js
+++ master.js
@@ -1,25 +1,72 @@
 var project = { 
   "9th Grade": [
-      
+    { 
+      "title": "The Science of Color: Light",
+      "description": "This is my first project for APP Academy. We were allowed to make a website about any subject we desire. The only catch was that we must present it to the class later.",  
+    },
+    { 
+      "title": "Sailor at Sea",
+      "description": "sailor at Sea an introduction to SVG animation. Because it is HTML only, the animation is quite crude and uninteractive.",  
+    },
+    { 
+      "title": "Tic-Tac-Toe",
+      "description": "Tic-Tac-Toe is the first game I made in APP Academy. Eventhough it was severely inefficent, I was proud that the game could decide the winner by itself.",  
+    },
+    { 
+      "title": "Mouse's Road",
+      "description": "Mouse's road was my final freshman year APP Academy project. Like our first, we get to pick our topic. I decided to make a game becasue filling a webpage with content is bothersome.",  
+    },
   ],
   "10th Grade": [
     { 
+      "title": "Lamplit Night",
+      "description": "This is my first personal project. I originally planned it to be a whack-a-mole game like in Plants Vs. Zombies, but I made changes to the plan and now it is a shooting game.",  
+    },
+    { 
+      "title": "Guessing Game",
+      "description": "Guessing game is a typical number guessing game ranging from 1-100. The base page layout was designed by Ms. Johnson, the sophomore APP Academy teacher.",  
+    },
+    { 
+      "title": "Compliment Generator",
+      "description": "Compliment generator is a compliment generator as the name suggest. Its adjective and dessert selections are randomly chosen from a manually embedded array.",  
+    },
+    { 
+      "title": "AP Flashcards",
+      "description": "AP flashcards is a simple flashcard application with flag property. However, adding flashcards require manually adding the text into the code because this is client-side only.",  
+    },
+    { 
+      "title": "College Plan",
+      "description": "Collge plan is the final project for the first semester of sophomore year. We were suppose to name our favorite colleges, but I had no clue about that at he time, so I randomly picked 3.",  
+    },
+    { 
+      "title": "Movie Lookup",
+      "description": "Movie Lookup is an introduction to API. We are suppose to aaccess a movie source and display specific informations like author and cover art.",  
+    },
+    { 
+      "title": "Save thee Man",
+      "description": "Save thee man was a voluntary mandatory project for the top 5% of the class. Unfortunately, I got selected. It is a fairly simple hang man game for 2 players.",  
+    },
+    { 
+      "title": "Earthquakes",
+      "description": "Earthquake is an API practice project. It required a code sign up before accessing the database.",  
+    },
+    { 
+      "title": "Weather Wardrobe",
+      "description": "Weather wardrobe is an API project. We get the weather of the desired location and suggest clothing articles based on the result. This is for men only and/or tomboys.",  
+    },
+    { 
       "title": "Cart of the Future",
-      "description": "Hello Worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",  
+      "description": 'Cart of the future is my final project for sophomore year. We are suppose to "plan" our future life and determine its cost. I kind of made it more generalized than specific to my lifestyle.',  
     },
     { 
       "title": "Ca-Ro",
-      "description": "Hello Worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",  
-    },
-    { 
-      "title": "Lamplit Night",
-      "description": "Hello Worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",  
+      "description": "Caro is a personal project I did for fun to practice my coding skill. It is based on a Vietnamese game and very similar to Tic-Tac-Toe. The objective is to get 5-in-a-row.",  
     },
   ],
   "11th Grade": [
     { 
       "title": "Snake",
-      "description": "Helsdkvlnskdlndddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",  
+      "description": "This is a project I worked on during the last week of summer. It is a simple snake game meant to refresh my coding skill.",  
     },
   ],
   "12th Grade": [
@@ -32,19 +79,54 @@ var codePen = {
     { 
       "title": "SVG Key",
       "link": "XpVOEW?editors=0010",  
-      "description": "Hellddddddddddd",  
+      "description": "Svg key is a codepen I made as a  quick reference to the svg functions that Ms. Squire shared in freshman year.",  
+    },
+    { 
+      "title": "Pineapple Nation",
+      "link": "LWPoGW",  
+      "description": "Pineapple nation is a simple stampinator codepen project. The only objective is to stamp the canvas.",  
+    },
+    { 
+      "title": "Flappy Fish",
+      "link": "wJJKLW?editors=1000",  
+      "description": "Flappy fish is a very simple and buggy flappy bird variation. Not to mention, it is ridiculously repetitive.",  
+    },
+    { 
+      "title": "Pikadventure",
+      "link": "mWzBRb",  
+      "description": "Pikadventure is my ridiculously simple sidescroller game. It mainly highlight the Pokemon franchise by Game Freak as one of my personal favorite.",  
     },
     { 
       "title": "Polygon Assist v.2",
       "link": "mqdKZx?editors=0010",  
-      "description": "Hello Worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",  
+      "description": "Polygon Assist v.2 is a simple codepen that allow one to draw shape by connecting dots. The coordinates are actually displayed in the console and not on the actual screen.",  
     },
   ],
   "10th Grade": [
     { 
+      "title": "Mad Libs",
+      "link": "ZXGwaL?editors=1100",  
+      "description": "This is a simple mad lib codepen I made. I had fun doing, but who wouldn't? It is meant to be hilarious and entertaining.",  
+    },
+    { 
+      "title": "Flash Art",
+      "link": "veWGOy?editors=0110",  
+      "description": "Flash art an svg animation done mostly with Javascript. It also highlight CW Flash as one of my favorite TV show.",  
+    },
+    { 
+      "title": "Times Table",
+      "link": "EoMrpo?editors=0010",  
+      "description": "Times table is a relatively basic practice for 'for loop' and 'input'. There really isn't that much to say beyond that.",  
+    },
+    { 
       "title": "Movable ViewBox",
       "link": "mxRBRp?editors=0010",  
-      "description": "Hello Worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",  
+      "description": "Movable viewbox was made based on an idea suggested by my classmate, Liam Dimas. He suspected that the viewBox property can be manipulated by Javascript. He was right.",  
+    },
+    { 
+      "title": "Revealing Ring",
+      "link": "PRmboW?editors=0110",  
+      "description": "Revealing ring is a codepen that demonstrate using the 'clip' property with javscript. As a bonus, I also incorporated viewBox manipulatuion into the codepen.",  
     },
   ],
   "11th Grade": [
@@ -79,17 +161,17 @@ function loadProject(level){
       //var placeHolderText2 = 'displayDescription(0, ' + row + ", " + column + ")";  
       //var placeHolderText3 = 'hideDescription(0, ' + row + ", " + column + ")";  
       if(column >= 2){
-        text += '<div class="col-md-3 description" onclick="hideDescription(0, ' + row + ', ' + column + ')" id="ProjectDescription' + row + '_' + column + '"><p>&emsp;' + project[level][0].description +'</p><br><center><a href="https://pineapplesofjustice.github.io/' + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '/" target="_blank"><button class="linkButton" id="ProjectLinkButton' + row + '_' + column + '">Open</button></a></center></div>'; 
-        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(0, ' + row + ', ' + column + ')" id="ProjectMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/Project/' + level + "/" + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '.PNG" alt="' + project[level][0].title + '" class="thumbnail"/><h2>' + project[level][0].title +'</h2></center></div>'; 
+        text += '<div class="col-md-3 description" onclick="hideDescription(0, ' + row + ', ' + column + ')" id="ProjectDescription' + row + '_' + column + '"><p>&emsp;' + project[level][0].description +'</p><br><center><a href="https://pineapplesofjustice.github.io/' + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "") + '/" target="_blank"><button class="linkButton" id="ProjectLinkButton' + row + '_' + column + '">Open</button></a></center></div>'; 
+        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(0, ' + row + ', ' + column + ')" id="ProjectMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/Project/' + level + "/" + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "") + '.PNG" alt="' + project[level][0].title + '" class="thumbnail"/><h2>' + project[level][0].title +'</h2></center></div>'; 
       }  
       else{  
-        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(0, ' + row + ', ' + column + ')" id="ProjectMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/Project/' + level + "/" + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '.PNG" alt="' + project[level][0].title + '" class="thumbnail"/><h2>' + project[level][0].title +'</h2></center></div>';  
-        text += '<div class="col-md-3 description" onclick="hideDescription(0, ' + row + ', ' + column + ')" id="ProjectDescription' + row + '_' + column + '"><p>&emsp;' + project[level][0].description +'</p><br><center><a href="https://pineapplesofjustice.github.io/' + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '/" target="_blank"><button class="linkButton" id="ProjectLinkButton' + row + '_' + column + '">Open</button></a></center></div>';   
+        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(0, ' + row + ', ' + column + ')" id="ProjectMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/Project/' + level + "/" + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "") + '.PNG" alt="' + project[level][0].title + '" class="thumbnail"/><h2>' + project[level][0].title +'</h2></center></div>';  
+        text += '<div class="col-md-3 description" onclick="hideDescription(0, ' + row + ', ' + column + ')" id="ProjectDescription' + row + '_' + column + '"><p>&emsp;' + project[level][0].description +'</p><br><center><a href="https://pineapplesofjustice.github.io/' + project[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "") + '/" target="_blank"><button class="linkButton" id="ProjectLinkButton' + row + '_' + column + '">Open</button></a></center></div>';   
       }
       column += 1;
       project[level].shift();
       if(column == 4){
-        document.getElementById("projectSpace").innerHTML += '</div>';  
+        text += '</div>';  
         row += 1;  
         column = 0;  
       }  
@@ -121,17 +203,17 @@ function loadCodePen(level){
       //var placeHolderText2 = 'displayDescription(0, ' + row + ", " + column + ")";  
       //var placeHolderText3 = 'hideDescription(0, ' + row + ", " + column + ")";  
       if(column >= 2){
-        text += '<div class="col-md-3 description" onclick="hideDescription(1, ' + row + ', ' + column + ')" id="CodePenDescription' + row + '_' + column + '"><p>&emsp;' + codePen[level][0].description +'</p><br><center><a href="https://pineapplesofjustice.github.io/' + codePen[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '/" target="_blank"><button class="linkButton" id="CodePenLinkButton' + row + '_' + column + '">Open</button></a></center></div>'; 
-        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(1, ' + row + ', ' + column + ')" id="CodePenMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/CodePen/' + level + "/" + codePen[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '.PNG" alt="' + codePen[level][0].title + '" class="thumbnail"/><h2>' + codePen[level][0].title +'</h2></center></div>';          
+        text += '<div class="col-md-3 description" onclick="hideDescription(1, ' + row + ', ' + column + ')" id="CodePenDescription' + row + '_' + column + '"><p>&emsp;' + codePen[level][0].description +'</p><br><center><a href="https://codepen.io/PineappleMaster2002/pen/' + codePen[level][0].link + '" target="_blank"><button class="linkButton" id="CodePenLinkButton' + row + '_' + column + '">Open</button></a></center></div>'; 
+        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(1, ' + row + ', ' + column + ')" id="CodePenMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/CodePen/' + level + "/" + codePen[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "") + '.PNG" alt="' + codePen[level][0].title + '" class="thumbnail"/><h2>' + codePen[level][0].title +'</h2></center></div>';          
       }  
       else{  
-        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(1, ' + row + ', ' + column + ')" id="CodePenMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/CodePen/' + level + "/" + codePen[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "-") + '.PNG" alt="' + codePen[level][0].title + '" class="thumbnail"/><h2>' + codePen[level][0].title +'</h2></center></div>';  
+        text += '<div class="col-md-3 main" ondblclick=' + placeHolderText1 + ' onclick="displayDescription(1, ' + row + ', ' + column + ')" id="CodePenMain' + row + '_' + column + '"><center><img src="Images/Thumbnail/CodePen/' + level + "/" + codePen[level][0].title.replace(/\s/g, "-").replace(/'/g, "-").replace(/:/g, "") + '.PNG" alt="' + codePen[level][0].title + '" class="thumbnail"/><h2>' + codePen[level][0].title +'</h2></center></div>';  
         text += '<div class="col-md-3 description" onclick="hideDescription(1, ' + row + ', ' + column + ')" id="CodePenDescription' + row + '_' + column + '"><p>&emsp;' + codePen[level][0].description +'</p><br><center><a href="https://codepen.io/PineappleMaster2002/pen/' + codePen[level][0].link + '" target="_blank"><button class="linkButton" id="CodePenLinkButton' + row + '_' + column + '">Open</button></a></center></div>';   
       }
       column += 1;
       codePen[level].shift();
       if(column == 4){
-        document.getElementById("codePenSpace").innerHTML += '</div>';  
+        text += '</div>';  
         row += 1;  
         column = 0;  
       }  
